3f3e66fd84231c8955a62d28ce16e9c6fb88c3b0
Orsolya Totik <ototik@gmail.com>
2020-04-05 17:55:01 +0200

    Quest 020

--- Front/homersodyssey/src/components/SignUp.js
+++ Front/homersodyssey/src/components/SignUp.js
@@ -4,23 +4,87 @@ class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "test@test.com",
+      email: "",
+      password: "",
+      passwordre: "",
+      firstname: "",
+      lastname: "",
     };
-    this.updateEmailField = this.updateEmailField.bind(this);
   }
 
   updateEmailField = (event) => {
+    const target = event.target;
+    const stateName = target.name;
+
+    const stateValue = target.value;
+
     this.setState({
-      email: event.target.value,
+      [stateName]: stateValue,
     });
+  };
+
+  handleSubmit = (event) => {
+    console.log(JSON.stringify(this.state), "console.log");
+    event.preventDefault();
   };
 
   render() {
     return (
       <div>
-        <h1>{this.state.email}</h1>
-
-        <input onChnage={this.updateEmailField} type="email" name="email" />
+        <h1>{JSON.stringify(this.state)}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <span>
+            First Name
+            <input
+              onChange={this.updateEmailField}
+              type="text"
+              name="firstname"
+              value={this.state.firstname}
+            />
+          </span>
+          <br />
+          <span>
+            Last Name
+            <input
+              onChange={this.updateEmailField}
+              type="text"
+              name="lastname"
+              value={this.state.lastname}
+            />
+          </span>
+          <br />
+          <span>
+            E-mail
+            <input
+              onChange={this.updateEmailField}
+              type="email"
+              name="email"
+              value={this.state.email}
+            />
+          </span>
+          <br />
+          <span>
+            Password
+            <input
+              onChange={this.updateEmailField}
+              type="password"
+              name="password"
+              value={this.state.password}
+            />
+          </span>
+          <br />
+          <span>
+            Re-Password
+            <input
+              onChange={this.updateEmailField}
+              type="passwordconf"
+              name="passwordconf"
+              value={this.state.passwordconf}
+            />
+          </span>
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
